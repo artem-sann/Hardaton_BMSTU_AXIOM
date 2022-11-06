a7782689ac01c3a0226ac91188a3d849a554bbd2
Artem Sannikov <artemksannikov@gmail.com>
2022-11-06 18:36:42 +0300

    LCD work and spi cross-transfer

--- Arduino_Nano_code/Arduino_Nano_code.ino
+++ Arduino_Nano_code/Arduino_Nano_code.ino
@@ -1,11 +1,76 @@
+#include <LiquidCrystalRus.h>
+#include<SPI.h>
 
+constexpr uint8_t PIN_RS = 3;
+constexpr uint8_t PIN_EN = 4;
+constexpr uint8_t PIN_DB4 = 5;
+constexpr uint8_t PIN_DB5 = 6;
+constexpr uint8_t PIN_DB6 = 7;
+constexpr uint8_t PIN_DB7 = 8;
+
+// Массив корторый приходит от мастера
+volatile uint8_t master_arr [7]; // ex: "0001000" - тип топлива и количество
+// Массив который отдаем от мастеру
+volatile uint8_t slave_arr  [7];
+// счетчик входящих байт
+volatile int16_t countSPIb = -1;
+
+bool start_fuel = false;
+
+LiquidCrystalRus lcd(PIN_RS, PIN_EN, PIN_DB4, PIN_DB5, PIN_DB6, PIN_DB7);
 
 void setup() {
-  // put your setup code here, to run once:
+  //---------------------------------LCD setup---------------------------------------------->
+  lcd.begin(16, 2);
+  // Устанавливаем курсор в колонку 0 и строку 0
+  lcd.setCursor(0, 0);
+  // Печатаем первую строку
+  lcd.print("Отскан. QR код");
+  // Устанавливаем курсор в колонку 0 и строку 1
+  lcd.setCursor(0, 1);
+  // Печатаем вторую строку
+  lcd.print("Поиск бензобака");
+  //----------------------------------------------------------------------------------------->
+  Serial.begin(9600);
+  //---------------------------------SPI setup----------------------------------------------->
+  // Настройка SPI как SLAVE
+  DDRB|=(1<<PB4);                        // Настроить вывод MISO на выход
+  SPCR |= (1 << SPIE)|(1 << SPE);        // Прерывание включено и сам SPI как SLAVE
 
+  // отладка
+  slave_arr [0] = 100;
+  slave_arr [1] = 200;
+  slave_arr [2] = 250;
+  //----------------------------------------------------------------------------------------->
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+ISR (SPI_STC_vect)                       // Прерывание SPI - пришел байт
+{
+  if (countSPIb < 0) {                   // пришла "пустышка"
+    countSPIb++;                         // увеличивам счетчик
+    SPDR = slave_arr [countSPIb];        // подгружаем нулевой байт массива ведомого
+    return;                              // выходим из процедуры
+  }
+
+  master_arr [countSPIb] = SPDR;         // получаем байт от мастера
+  countSPIb++;                           // увеличиваем счетчик
+  SPDR = slave_arr [countSPIb];          // отдаем байт ведомого (+1 индекс)
+
+  if (countSPIb >= sizeof(master_arr)) { // если кончился массив
+    countSPIb = -1;                      // обнуляем счетчик и ждем следующий обмен
+  }
+  start_fuel = true;
+}                                        // Конец SPI - пришел байт
+
+
+void loop() {
+  if (start_fuel) {
+    lcd.setCursor(0, 1);
+    lcd.clear();
+    lcd.print("Начинаем запр.");
+    Serial.println("done");
+    delay(100);
+  }
+  
 }
--- Arduino_UNO_code/Arduino_UNO_code.ino
+++ Arduino_UNO_code/Arduino_UNO_code.ino
@@ -1,10 +1,62 @@
-void setup() {
-Serial.begin(9600);
+// Данный код будет работать только на платах с чипом ATmega328P например
+// Arduino UNO, NANO
+// Он использует интрефейс SPI на пинах
+// 13 - SCK, 12 - MISO, 11 - MOSI, 10 - SS
+
+// Массивы могут иметь произвольную длинну
+// Но должны совпадать по количеству байт
+// Не использованные байты можно оставлять пустыми
+
+// Массив который отправляем ведомому
+uint8_t master_arr [7];
+// Массив корторый получаем от ведомого
+uint8_t slave_arr  [7];
+
+void setup (void)
+{
+  // отладка
+  Serial.begin (115200);
+  Serial.println ();
+  master_arr [0] = 0;
+  master_arr [1] = 0;
+  master_arr [2] = 0;
+  master_arr [3] = 1;
+  master_arr [4] = 0;
+  master_arr [5] = 0;
+  master_arr [6] = 0;
+  
+  // Настройка SPI как мастера
+  DDRB |= (1 << PB2) | (1 << PB3) | (1 << PB5);   // Настроить выводы MOSI,SS,SCK на выход            
+  PORTB |= (1 << PB2);                            // Установить "1" на линии SS
+  /* Enable SPI, Master, set clock rate fck/16 */
+  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
-void loop() {
-if (Serial.available() > 0) {
-String data = Serial.readStringUntil('\n');
-Serial.print("You sent me: ");
-Serial.println(data);
-}
+
+
+void refreshSPI ()                        // Процедура обмена массивами между платами
+{
+  PORTB &= ~(1 << PB2);                   // Установить "0" на линии SS1
+  SPDR = 0xFF;                            // Отправляем "пустышку" для загрузки байта slave_arr[0]
+  while (!(SPSR & (1 << SPIF))) ;         // Дождаться окончания передачи
+  delayMicroseconds (20);                 // Пауза чтобы успел подгрузиться байт [0] на ведомом
+  // цикл по master_arr[]
+  for (uint8_t i = 0; i < sizeof(master_arr); i++) {
+    SPDR = master_arr[i];                 // Отправить байт
+    while (!(SPSR & (1 << SPIF))) ;       // Дождаться окончания передачи
+    slave_arr[i] = SPDR;                  // Принять байт
+    delayMicroseconds (20);               // Пауза чтобы успел подгрузиться байт на ведомом
+  }
+  PORTB |= (1 << PB2);                    // Установить "1" на линии SS
+}                                         // Конец процедуры обмена
+
+
+void loop (void)
+{
+  delay (1000);
+  refreshSPI ();
+  // цикл по slave_arr[]
+  for (int i = 0; i < sizeof(slave_arr); i++) {
+    Serial.println(slave_arr[i], DEC);
+  }
+  Serial.println('-');
 }
