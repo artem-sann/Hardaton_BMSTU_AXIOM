67c3796eb15a13cdbad6bd11a9b497c3a8f937c6
Artem Sannikov <artemksannikov@gmail.com>
2022-11-07 03:06:02 +0300

    Arduino UNO parser from paspberry

--- Arduino_UNO_code/Arduino_UNO_code.ino
+++ Arduino_UNO_code/Arduino_UNO_code.ino
@@ -12,13 +12,23 @@ uint8_t master_arr [7];
 // Массив корторый получаем от ведомого
 uint8_t slave_arr  [7];
 
+String string_from_rasp = "+000;+000;+000;+000;00;00"; // srvo1;servo2;step1;step2;fuel_type;fuel_volume
+
+int delta_srv1 = 0;
+int delta_srv2 = 0;
+int delta_step1 = 0;
+int delta_step2 = 0;
+uint8_t fuel_typ = 0;
+uint8_t fuel_vol = 0;
+
+
 void setup (void)
 {
   // отладка
   Serial.begin (115200);
   Serial.println ();
   master_arr [0] = 92;
-  master_arr [1] = 87;
+  master_arr [1] = 46;
   master_arr [2] = 0;
   master_arr [3] = 1;
   master_arr [4] = 0;
@@ -47,16 +57,57 @@ void refreshSPI ()                        // Процедура обмена м�
     delayMicroseconds (20);               // Пауза чтобы успел подгрузиться байт на ведомом
   }
   PORTB |= (1 << PB2);                    // Установить "1" на линии SS
-}                                         // Конец процедуры обмена
+} 
+
+
+void message_parser(String input, int *delta_srv1, int *delta_srv2, int *delta_step1, int *delta_step2, uint8_t *fuel_typ, uint8_t *fuel_vol) {
+  if (input[0] == '+'){
+    *delta_srv1 = (input[1]-48)*100 + (input[2]-48)*10 + (input[3]-48);
+  }
+  else {
+    *delta_srv1 = -(input[1]-48)*100 - (input[2]-48)*10 - (input[3]-48);   
+  }
+
+  if (input[5] == '+'){
+    *delta_srv2 = (input[6]-48)*100 + (input[7]-48)*10 + (input[8]-48);
+  }
+  else {
+    *delta_srv2 = -(input[6]-48)*100 - (input[7]-48)*10 - (input[8]-48);   
+  }
+
+  if (input[10] == '+'){
+    *delta_step1 = (input[11]-48)*100 + (input[12]-48)*10 + (input[13]-48);
+  }
+  else {
+    *delta_step1 = -(input[11]-48)*100 - (input[12]-48)*10 - (input[13]-48);   
+  }
+
+  if (input[15] == '+'){
+    *delta_step2 = (input[16]-48)*100 + (input[17]-48)*10 + (input[18]-48);
+  }
+  else {
+    *delta_step2 = -(input[16]-48)*100 - (input[17]-48)*10 - (input[18]-48);   
+  }
+
+  *fuel_typ = (input[20]-48)*10 + (input[21]-48);
+  *fuel_vol = (input[23]-48)*10 + (input[24]-48);
+}                                        
 
 
 void loop (void)
 {
-  delay (5000);
+  delay (1000);
   refreshSPI ();
-  // цикл по slave_arr[]
-  for (int i = 0; i < sizeof(slave_arr); i++) {
-    Serial.println(slave_arr[i], DEC);
+
+  if (Serial.available() > 0) {
+    string_from_rasp = Serial.readStringUntil('\n');
+    message_parser(string_from_rasp, &delta_srv1, &delta_srv2, &delta_step1, &delta_step2, &fuel_typ, &fuel_vol);
   }
-  Serial.println('-');
+  
+
+  master_arr [0] = fuel_typ;
+  master_arr [1] = fuel_vol;
+
+  Serial.println(delta_step2);
+
 }
